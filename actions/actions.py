fa39071974f305937a190ec93d3f05c7eb7639b6
ongraphpythondev <pythondev@ongraph.com>
2022-07-19 13:56:54 +0530

    Some updation

--- actions/actions.py
+++ actions/actions.py
@@ -19,19 +19,17 @@ class ActionCheckWeather(Action):
         return "action_get_weather"
     
     def run(self, dispatcher, tracker, domain):
-        print("Vinayy ")
         api_key = '1a1c81af300a919cbc5225327e83dca2'
-        print("fdsfjsbdfjfbsjb")
         loc = tracker.get_slot('location')
-        current = requests.get('http://api.openweathermap.org/data/2.5/weather?q={}&appid={}'.format(loc, api_key)).json()
-        print(current, "current")
+        current = requests.get('http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric'.format(loc, api_key)).json()
+        msg = f"Weather of - {loc}"
         country = current['sys']['country']
         city = current['name']
         condition = current['weather'][0]['main']
         temperature_c = current['main']['temp']
         humidity = current['main']['humidity']
-        wind_mph = current['wind']['spped']
-        response = """It is currently {} in  {} at the moement. The temperature is {} degrees, the humidity is {}% and the wind speed is {} mph.""".format(condition, city, temperature_c, humidity, wind_mph)
+        wind_mph = current['wind']['speed']
+        response = msg + "\n"+"""It is currently {} in  {} at the moement. The temperature is {} degrees, the humidity is {}% and the wind speed is {} mph.""".format(condition, city, temperature_c, humidity, wind_mph)
         dispatcher.utter_message(response)
         return [SlotSet('location', loc)]
 
